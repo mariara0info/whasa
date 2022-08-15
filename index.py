08430686b064b044c236217d62d585cb1309d548
mariara0info <52378134+mariara0info@users.noreply.github.com>
2022-08-14 22:52:03 -0400

    Add files via upload

--- index.py
+++ index.py
@@ -35,35 +35,22 @@ def uploader():
   # Retornamos una respuesta satisfactoria
   resul12 = resuelvelo()
   return render_template('Result_for_Textbox.html', SName =   ' , '.join(resul12)) 
-  #return "<h1>Archivo subido exitosamente</h1>"
-  # SName =   '  '.join(resul12))    
-
 def resuelvelo():
      df = pd.read_csv('./Archivos/Whatlinks.txt')
      lola= np.asarray(df)
-     #chat= list()
-     linder=list()
-     #image1=list()
-     archivo = open('./Whatgrupos.txt', 'w' , encoding='UTF8')
-
+     linder=list() 
      for s in lola:
           listToStr = ' '.join([str(elem) for elem in s]) 
           #print(listToStr)
           r = requests.get(listToStr)
-          soup = bs(r.content, 'html.parser')
-
+          soup = bs(r.content, 'lxml')
           # Locate the box that contains title and transcript
           box = soup.find('div', class_='_9vd6 _9t33 _9bir _9bj3 _9bhj _9v12 _9tau _9tay _9u6w _9se- _9u5y')
           # Locate title and transcript
-          first_header = box.find('h3').get_text( )
-     
-          if len(first_header) != 0:
-           #linder.append(listToStr.strip(' '))          
-           linder.append(listToStr)
-           archivo.write(first_header +'\n')
-           archivo.write(listToStr +'\n')
-  
-     archivo.close()
+          first_header = box.find('h3').get_text()     
+          if len(first_header) != 0:    
+           linder.append(listToStr) 
+           
      return linder
     
 if __name__ == '__main__':
